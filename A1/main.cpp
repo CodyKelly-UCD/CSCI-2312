3dd23a4dcaa70e2690177b8e31242359337c7ac0
Cody Kelly <codykellydesign@gmail.com>
2017-08-24 18:10:23 -0600

    Added makefile, simplified (?) code

--- A1/main.cpp
+++ A1/main.cpp
@@ -7,30 +7,32 @@ const int TABLEWIDTH = 30;
 
 int GetInput()
 {
-	int input = 0;
 
-	cout << "Please input the number of plants in inventory (input -1 to quit): ";
-	cin >> input;
-
-	return input;
-}
-
-void FormatOutput(string msg, int plants)
-{
-	cout << setw(TABLEWIDTH) << left << msg;
-	cout << setw(TABLEWIDTH) << left << plants << endl;
 }
 
 void CalculatePlants(int input) 
 {
+	// Header
 	cout << "\n\nPlant sales of " << input << " plants:\n\n";
+
+	// Column names
 	cout << setw(TABLEWIDTH) << left << "Outdoor temp:" << "Number of plants sold:" << endl;
+
+	// Divider
 	cout << setw(TABLEWIDTH) << setfill('-') << '-' << setw(TABLEWIDTH) << '-' << endl << setfill(' ');
-	FormatOutput("Colder than 40 degrees:", input * 0.1);
-	FormatOutput("Between 40 and 60 degrees:", input * 0.3);
-	FormatOutput("Between 61 and 70 degrees:", input * 0.5);
-	FormatOutput("Between 71 and 80 degrees:", input * 0.6);
-	FormatOutput("Hotter than 80 degrees:", input * .4);
+
+	// Data
+	cout << setw(TABLEWIDTH) << left << "Colder than 40 degrees:";
+	cout << setw(TABLEWIDTH) << left << (int)(input * 0.1);
+	cout << setw(TABLEWIDTH) << left << "Between 40 and 60 degrees:";
+	cout << setw(TABLEWIDTH) << left << (int)(input * 0.3);
+	cout << setw(TABLEWIDTH) << left << "Between 61 and 70 degrees:";
+	cout << setw(TABLEWIDTH) << left << (int)(input * 0.5);
+	cout << setw(TABLEWIDTH) << left << "Between 71 and 80 degrees:";
+	cout << setw(TABLEWIDTH) << left << (int)(input * 0.6);
+	cout << setw(TABLEWIDTH) << left << "Hotter than 80 degrees:";
+	cout << setw(TABLEWIDTH) << left << (int)(input * .4);
+
 	cout << endl;
 }
 
@@ -40,12 +42,15 @@ int main()
 
 	cout << "Welcome to the Plant Sale Estimator\n\n";
 
-	input = GetInput();
+	cout << "Please input the number of plants in inventory (input -1 to quit): ";
+	cin >> input;
 
 	while (input != -1)
 	{
 		CalculatePlants(input);
-		input = GetInput();
+
+		cout << "Please input the number of plants in inventory (input -1 to quit): ";
+		cin >> input;
 	}
 
 	return 0;
