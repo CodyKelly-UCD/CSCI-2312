03668052bc9cc6abef52ad3295164f44f991622f
Cody Kelly <cody.kelly@ucdenver.edu>
2017-10-25 15:42:55 -0600

    Updated included libraries in HW5

--- kellyHW5/main.cpp
+++ kellyHW5/main.cpp
@@ -5,6 +5,7 @@
  2312
 */
 
+#include <algorithm>
 #include <iostream>
 #include <iomanip>
 #include <vector>
@@ -94,7 +95,7 @@ void printMap(Node* currentRoom)
     }
     else
     {
-        cout << "\n\n\n";
+        cout << "\n\n";
     }
 }
 
